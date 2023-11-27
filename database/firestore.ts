a43ba149ec0c41bc3d086a1a7e11ad4114f4f734
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-11-27 11:21:23 -0600

    add firestore log files to gitingore and added db code to firestore.ts

--- database/firestore.ts
+++ database/firestore.ts
@@ -1,12 +1,32 @@
-const { initiallizeApp, cert } = require('firebase-admin/app');
-import getFirestore from 'firebase-admin/firestore'
+//Conection to firebase cloud
+
+/*
+const { initializeApp, cert } = require('firebase-admin/app')
+const { getFirestore } = require('firebase-admin/firestore')
+
+const serviceAccount = require('./key.json')
+
+initializeApp({
+    credential: cert(serviceAccount)
+})
+
+const db = getFirestore()
+
+*/
 
 
 
+//============================================================================
+// connection for firestore emulator
 
 
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+
+// firebaseApps previously initialized using initializeApp()
+const db = getFirestore();
+connectFirestoreEmulator(db, '127.0.0.1', 8080);
 
 
-
+module.exports = { db }
 
 
