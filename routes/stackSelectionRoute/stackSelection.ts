8ae7f4bfad73779e6ff42782027498d14d99fa08
Peder <116122618+pedergoodman@users.noreply.github.com>
2023-11-30 14:24:35 -0600

    added get route for stackSelectionRoute

--- routes/stackSelectionRoute/stackSelection.ts
+++ routes/stackSelectionRoute/stackSelection.ts
@@ -1,9 +1,19 @@
-import express, { Request, Response } from 'express';
+// Tools
+import express, { Request, Response } from "express";
 const router = express.Router();
 
-router.get('/', (req, res) => {
+// Dummy Data
+import selection from "../../testing/db/stack-selection.json";
 
-  
-})
+// * ROUTES
 
-export default router
+router.get("/:userid", (req, res) => {
+  //query the database to check if the user id is valid
+  if (req.params.userid !== "string1") {
+    res.sendStatus(403);
+  }
+  // Send status 200 and array of strings
+  res.status(200).send(selection);
+});
+
+export default router;
