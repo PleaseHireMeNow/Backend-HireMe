d48484a960bf7de362fd9feba6bdf275cdf85a8b
Zach Garrison <zgedina@yahoo.com>
2023-11-30 14:32:50 -0600

    user route testing

---
+++ routes/userRoute/userRoute.test.ts
@@ -0,0 +1,39 @@
+import express, { Application, Request, Response } from "express";
+import user from '../userRoute/userRoute'
+
+const app = require("../../server");
+const request = require("supertest");
+const usersInDatabase = require("../../testing/db/user.json");
+
+
+
+describe("User route test", () => {
+    test("The get route with userID provided, should return user information", done => {
+        request(app)
+            .get(`/api/user/${usersInDatabase[0].userId}`)
+            .end((err: Object, res: Response) => {
+                if (err) {
+                    console.error("err", err)
+                }
+                expect(res.status).toBe(200)
+
+                done()
+            })
+
+    })
+
+    test.only("This should return a 403 when given an incorrect user ID", done => {
+        request(app)
+            .get(`/api/user/${usersInDatabase[1].userId}`)
+            .end((err: Object, res: Response) => {
+
+                if (err) {
+                    console.error("err", err)
+                }
+                expect(res.status).toBe(403)
+                done()
+            })
+    }, 1000)
+})
+
+
--- routes/userRoute/userRoute.ts
+++ routes/userRoute/userRoute.ts
@@ -7,9 +7,18 @@ router.get('/:userId', (req: Request, res: Response) => {
   const userId = req.params.userId;
 
 
+<<<<<<< Updated upstream
   if (!userId) {
     return res.status(404).send('User not found');
   }
+=======
+    if (userJson[0].userId === req.params.userId ) {
+      res.status(200).send(userJson[0].userId)
+   } else {
+      res.status(403)
+   }
+res.status(201)
+>>>>>>> Stashed changes
 
   res.status(200).json(userId);
 });
@@ -23,7 +32,11 @@ router.delete('/:userId', (req: Request, res: Response) => {
     return res.status(404).send('User not found');
   }
 
+<<<<<<< Updated upstream
   res.status(200).send('User data deleted successfully');
+=======
+
+>>>>>>> Stashed changes
 });
 
 
