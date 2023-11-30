85e30ad5dec61d578256cce64a3cfdaaac157bec
Peder <116122618+pedergoodman@users.noreply.github.com>
2023-11-30 15:59:00 -0600

    wrote tests for stackSelection POST route - with correct userid & [string] - with incorrect userid

--- routes/stackSelectionRoute/stackSelection.test.ts
+++ routes/stackSelectionRoute/stackSelection.test.ts
@@ -6,10 +6,12 @@ const app = require("../../server");
 const request = require("supertest");
 const usersInDatabase = require("../../testing/db/user.json");
 
-describe("stackSelection route test", () => {
-  const correctUserid = usersInDatabase[0].userId;
-  const incorrectUserid = "lkh34590dsa8fl4i3k5j";
+const correctUserid = usersInDatabase[0].userId;
+const incorrectUserid = "lkh34590dsa8fl4i3k5j";
 
+describe("stackSelection route test", () => {
+
+  // GET TESTS
   test("stackSelection GET route success test, should return array of strings & status 200", done => {
     // start dummy server
     request(app)
@@ -43,5 +45,36 @@ describe("stackSelection route test", () => {
   });
 
 
+// POST TESTS
+test("stackSelection POST route success test, should return array of strings and status 200", done => {
+  // start dummy server
+  request(app)
+    .post(`/api/stackSelection/${correctUserid}`)
+    .end((err: Object, res: Response) => {
+      
+      if (err) {
+        console.error(err);
+      }
+      expect(res.status).toBe(200)
+      if ("body" in res) {
+        expect(res.body).toEqual(selection);
+      }
+      done();
+    });
+});
+
+test("stackSelection POST route incorrect userid, should send 403", done => {
+  request(app)
+    .post(`/api/stackSelection/${incorrectUserid}`)
+    .expect(403)
+    .end((err: Object, res: Response) => {
+      if (err) {
+        console.error(err);
+        done(err)
+      } else {
+        done()
+      }
+    });
+});
 
 });
