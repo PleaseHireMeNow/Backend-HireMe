66383cdddda509db047c336ad81a721544222b01
Peder <116122618+pedergoodman@users.noreply.github.com>
2023-11-30 16:12:31 -0600

    wtore

--- routes/stackSelectionRoute/stackSelection.ts
+++ routes/stackSelectionRoute/stackSelection.ts
@@ -23,11 +23,16 @@ router.post("/:userid", (req, res) => {
       res.sendStatus(403);
     }
     // Send status 200 and array of strings
-    res.status(200).send(selection);
+    res.sendStatus(200);
 });
 
 router.put("/:userid", (req, res) => {
-  // res.sendStatus(200)
+    //query the database to check if the user id is valid
+    if (req.params.userid !== "string1") {
+      res.sendStatus(403);
+    }
+    // Send status 200 and array of strings
+    res.sendStatus(200);
 });
 
 router.delete("/:userid", (req, res) => {
