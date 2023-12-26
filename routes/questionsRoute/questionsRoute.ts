08c8745fc0aae21f9a60ada70bd1d1717d7c3d47
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-12-26 13:14:42 -0600

    fixed bug on userList.find method

--- routes/questionsRoute/questionsRoute.ts
+++ routes/questionsRoute/questionsRoute.ts
@@ -9,27 +9,26 @@ router.get("/:userid", async (req, res) => {
   // check if user id exists
   // get user id info from DB
 
-    // topic, difficulty
-    // previously answered questions 
+  // topic, difficulty
+  // previously answered questions 
   // call questions from DB
   // compare (check route methods)
   const userList = await getUsersInfo();
-  const matchingUser = userList.find(user => user.username = req.params.userid)
+  const matchingUser = userList.find(user => user.username === req.params.userid)
   console.log(matchingUser)
   if (
     //query the database to check if the user id is valid
-    !matchingUser
+    matchingUser?.username !== req.params.userid
   ) {
     res.sendStatus(403);
+  } else {
+    // pseudo code for looking up the user's topic(s) and difficulty(ies)
+    let topic = "JavaScript";
+    let difficulty = "entry-level";
+    //
+    const questions = await getNestedDocument("questions", topic, difficulty);
+    res.send(questions).status(200);
   }
-
-
-  // pseudo code for looking up the user's topic(s) and difficulty(ies)
-  let topic = "JavaScript";
-  let difficulty = "entry-level";
-  //
-  const questions = await getNestedDocument("questions", topic, difficulty);
-  res.send(questions).status(200);
 });
 
 export default router;
