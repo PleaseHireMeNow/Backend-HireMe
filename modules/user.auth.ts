15c5938dcbd53d2320cb112f02df62424bf20b30
Peder <116122618+pedergoodman@users.noreply.github.com>
2024-02-18 18:40:32 -0600

    added logic to check if a user has data in the database, if not it creates it

--- modules/user.auth.ts
+++ modules/user.auth.ts
@@ -1,6 +1,7 @@
 import * as admin from "firebase-admin";
 import { Request, Response, NextFunction } from "express";
-
+import { dbv2 } from "./db";
+import { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
 declare module "express-serve-static-core" {
   interface Request {
     user?: any; // You can replace `any` with a more specific type for your user object
@@ -12,21 +13,66 @@ export const validateUser = async (
   res: Response,
   next: NextFunction
 ) => {
-  // console.log(req.headers.authorization);
+  // parse bearer token from frontend
   const idToken = req.headers.authorization?.split(" ")[1] || "";
 
-  // testing ID token from Gabes acct
-  // let idToken =
-  // "eyJhbGciOiJSUzI1NiIsImtpZCI6ImFlYzU4NjcwNGNhOTZiZDcwMzZiMmYwZDI4MGY5NDlmM2E5NzZkMzgiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiR2FicmllbGxlIEdsYXNjbyIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQ2c4b2NLLVF4bXcyZXVOTmRYcXBRV3BlMnp5a2RLUnFLY1JHY19OckpHc3V5Rk9FQWFVPXM5Ni1jIiwiaXNzIjoiaHR0cHM6Ly9zZWN1cmV0b2tlbi5nb29nbGUuY29tL2hpcmVtZW5vdy1jODU0NiIsImF1ZCI6ImhpcmVtZW5vdy1jODU0NiIsImF1dGhfdGltZSI6MTcwODI5MTE3MywidXNlcl9pZCI6Imp1bkZNQmNJSlNPc1RPTWNsdDhkZ2lGRUNPaTEiLCJzdWIiOiJqdW5GTUJjSUpTT3NUT01jbHQ4ZGdpRkVDT2kxIiwiaWF0IjoxNzA4MjkxMTczLCJleHAiOjE3MDgyOTQ3NzMsImVtYWlsIjoiZ2FiZWdsYXNjb0BnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJnb29nbGUuY29tIjpbIjEwODY2MDY2NDQ1NzI5OTMwNTUwNiJdLCJlbWFpbCI6WyJnYWJlZ2xhc2NvQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6Imdvb2dsZS5jb20ifX0.CVR1wOXVlXmSIweLAfjJhizSKuw0MAby7I23rAVkqf2yYYIA_9x2maoDzlkiH-zFuQFHVgLUhQ2WdpLi1rBilGnb-bvDwDeK7UJRZLjujYuzSF2WBYeOcQFN0w_Wm3sa_rVO_ihFJsaMDfZKXnLjJhbkpi9-UmbwAe8hd_eZ_KbymPhWeIcyY-Ya6sqAiizCZEQIYyVtn_jsu6P54pvfMXsTUOVQloyz2ehNUJiWH9x8M_DOpo4-eRo7bubxAk1BV7MIIQzMKJuqDcBzCGd2W0jxWjtZlTpolhOe3nJyGPjuXn-r1pANTPF1qKmv6Kuh_iafhbQOUmMLQLAPavLkUg"
-
   try {
-    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    // decode Token from frontend
+    const decodedToken: DecodedIdToken = await admin
+      .auth()
+      .verifyIdToken(idToken);
+    // pass user data to route
+    // ? pass all of this or only necessary????
     req.user = decodedToken;
+
+    // returend user data
     console.log("token is valid. User is: ", decodedToken);
+
+    // * verify new user by checking if they have user in the DB
+    // check if uid exists in the DB, if not it's a new user
+    const userSnapshot = await dbv2
+      .collection("users")
+      .doc(decodedToken.uid)
+      .get();
+
+    console.log("userSnapshot is : ", userSnapshot.exists);
+
+    if (userSnapshot.exists) {
+      console.log("User has data in the database!");
+    } else {
+      console.log("This is a new user! Create a User in the DB");
+      createNewUserData(decodedToken);
+    }
 
     next();
   } catch (error) {
     console.log("Invalid User Token: ", error);
     res.status(401).json({ error: "Unauthorized" });
   }
+};
+
+const createNewUserData = async (decodedToken: DecodedIdToken) => {
+  console.log("made it to create new user");
+
+  const user_id = decodedToken.user_id;
+  const username = decodedToken.name;
+  const email = decodedToken.email;
+  const picture = decodedToken.picture;
+
+  let userData = {
+    user_id,
+    email,
+    username,
+    picture,
+    // ! we can add more initilization data here!
+  };
+
+  await dbv2.collection("users").doc(user_id).set(userData);
+
+  const userSnapshot = await dbv2
+    .collection("users")
+    .doc(decodedToken.uid)
+    .get();
+
+  console.log("userSnapshot is : ", userSnapshot.exists);
 };
